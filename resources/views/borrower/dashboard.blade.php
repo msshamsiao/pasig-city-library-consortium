2464f3c8ac849f1f42c3a265779003293114ef84
Samantha Siao <siao_samantha@plpasig.edu.ph>
2025-12-10 19:24:25 +0800

    Remove 'My Profile' quick action from borrower dashboard

--- resources/views/borrower/dashboard.blade.php
+++ resources/views/borrower/dashboard.blade.php
@@ -67,12 +67,6 @@
             </svg>
             <span class="font-medium text-gray-700">My Reservations</span>
         </a>
-        <a href="{{ route('profile.edit') }}" class="block px-6 py-4 bg-purple-50 hover:bg-purple-100 rounded-lg transition text-center">
-            <svg class="w-10 h-10 text-purple-600 mx-auto mb-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"/>
-            </svg>
-            <span class="font-medium text-gray-700">My Profile</span>
-        </a>
     </div>
 </div>
 
