a2664386a998f9865c0015443df8f7fe4e81b7ae
Samantha Siao <siao_samantha@plpasig.edu.ph>
2025-12-16 21:16:13 +0800

    Change logout icon to power/off button

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -197,7 +197,7 @@
                                 @csrf
                                 <button type="submit" class="p-2 text-red-600 hover:bg-red-50 rounded-full transition" title="Logout">
                                     <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"/>
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5.636 5.636a9 9 0 1012.728 0M12 3v9"/>
                                     </svg>
                                 </button>
                             </form>
